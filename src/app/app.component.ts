02d6cc0b601b45b9b64ebb2b9e1aab3a42086d2d
Lucas Gabriel <lucasgmnzs@gmail.com>
2024-06-27 22:05:05 -0300

    Add form title, fields and button, temporarily switching data when clicking on button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login-project';
+  
+  showLogin: boolean = true;
+
+  change() {
+    this.showLogin = !this.showLogin;
+  }
 }
